5542711276d73cae07a5ad1e045ce261784a5a5a
Critaz <46646792+engga86@users.noreply.github.com>
2023-10-31 18:09:19 +1100

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [amount, setAmount] = useState(0);
+  const [selected, setSelect] = useState(0);
+
+  const onSelect = (event) => {
+    setSelect(event.target.value);
+    // console.log(event);
+  };
+
+  const onChange = (event) => {
+    setAmount(event.target.value);
+  };
+  const onClick = () => {
+    setAmount(0);
+  };
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
+  // const coinPrice = coins.map((i) => i.quotes.USD.price);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>The Coins {loading ? "..." : `(${coins.length})`}</h1>
+      {loading ? <strong>Loading...</strong> : null}
+      <label>Enter amount of USD to spend</label> <br />
+      <input
+        value={amount}
+        onChange={onChange}
+        type="number"
+        placeholder="Enter number"
+      />
+      <br />
+      <br />
+      <select onChange={onSelect}>
+        <option key="-1">select coin</option>
+        {coins.map((i) => (
+          <option key={i.id} value={[i.quotes.USD.price]}>
+            {i.name} ({i.symbol}) : ${i.quotes.USD.price.toFixed(2)} USD
+          </option>
+        ))}
+      </select>
+      <br />
+      {/* {console.log(selected)} */}
+      <h3>
+        Currently you can buy:
+        {amount === 0 ? " 0" : (amount / selected).toFixed(2)} coins
+      </h3>
+      <select>
+        {coins.map((i) => (
+          <option key={i.id}>
+            {i.name} : {amount / i.quotes.USD.price} ({i.symbol})
+          </option>
+        ))}
+      </select>
+      <br />
+      <br />
+      <button onClick={onClick}>Reset</button>
     </div>
   );
 }
